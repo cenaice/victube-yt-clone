2c2a239042af9fd1c83222029b17e0e4720e536e
Victor <victorphiathep@gmail.com>
2023-09-07 17:06:52 -0400

    ignore firebase

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import SignIn from "./sign-in";
+
+
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <Link href="/">
         <Image width={50} height={10} src="/yt-logo.png" alt="Youtube Logo" />
       </Link>
+      <SignIn />
     </nav>
   );
 }
